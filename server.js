78db5a3958b7f57d4d50677c255678284a20a8c3
unknown <danielscarstensen@gmail.com>
2019-01-24 16:53:18 -0600

    cra and dependencies

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const db = require("./models")
+
+// Define Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve Static Assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cart-counter");
+// MLAB Link Below
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://DanC:123456@ds143953.mlab.com:43953/onthedob_db");
+
+const db = require('./models');
+
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
