d81c7894efb3da63ed9011c7da4e21b1605d7c96
unknown <danielscarstensen@gmail.com>
2019-02-18 15:35:28 -0600

    broken product list

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -1,57 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import {ProductConsumer} from '../context';
-import Game from './Game'
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import Game from './Game';
 
 
+import {ProductConsumer, ProductProvider} from '../context';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 100,
-  },
-  control: {
-    padding: theme.spacing.unit * 2,
-  },
-});
-
-class GameList extends React.Component {
-  state = {
-    spacing: '16',
-  };
+export default class ProductList extends Component {
 
   render() {
-    const { classes } = this.props;
-    const { spacing } = this.state;
+
+    // console.log(this.state.products)
 
     return (
-      <Grid container className={classes.root} spacing={16}>
-        <Grid item xs={12}>
-          <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
-            <ProductConsumer>
-                {[0, 1, 2].map(value => (
-                <Grid key={value.games}>
-                    <Paper className={classes.paper} />
+      <React.Fragment>
+          <Paper className='paper'>
+            <Grid container spacing={16}>
+              <Grid item>
+                <ButtonBase className='btn'>
+                  <img className='img' alt="complex" src="/static/images/grid/complex.jpg" />
+                </ButtonBase>
+              </Grid>
+                <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={16}>
+
+                  <Grid item xs>
+                    <Typography gutterBottom variant="subtitle1">
+                      Standard license
+                    </Typography>
+
+                    <Typography gutterBottom>
+                      Full resolution 1920x1080 • JPEG
+                    </Typography>
+
+                    <Typography color="textSecondary">
+                      ID: 1030114
+                    </Typography>
+                  </Grid>
+
+                  <Grid item>
+                    <Typography style={{ cursor: 'pointer' }}>
+                      Remove
+                    </Typography>
+                  </Grid>
                 </Grid>
-                ))}
-            </ProductConsumer>
-          </Grid>
-        </Grid>
-      </Grid>
-    );
-  }
+                <Grid item>
+                  <Typography variant="subtitle1">$19.00</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </React.Fragment>
+  )}
 }
+            
+            
+            
+    //         <div className='row'>
+    //         <ProductConsumer>
 
-GameList.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+    //           {/* this value comes from context.js. ProductConsumer returns value, callback function in value to map through data array */}
 
-export default withStyles(styles)(GameList);
+    //           {(value)=>{
+    //             return value.products.map( product => {
+    //               return <Product key={product.id} product={product} />;
+    //             })
+    //           }}
+    //         </ProductConsumer>
+    //         </div>;
+    //       </div>;
+    //   </React.Fragment>
+    // )
+
+
